f91eb99f1df5b8a4515d8641b3f8dc7d81c97c5e
SingareAkshay <singareakshay937@gmail.com>
2024-06-14 00:08:27 +0200

    Created new files

---
+++ 13-jun/LeafYear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LeafYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float leaf;
+
+        System.out.println("enter yers days");
+        leaf = sc.nextFloat();
+
+        String str = leaf % 4 == 0 ? "Its Leaf Year " : " Its Not Leaf Year ";
+        System.out.println(str);
+    }
+}
