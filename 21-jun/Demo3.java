bb7de17cff19b94b873341ca46c7a8be42975b49
SingareAkshay <singareakshay937@gmail.com>
2024-06-23 00:08:45 +0200

    new files are added

---
+++ 21-jun/Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3{
+  public static void main(String x[]){
+
+	int var1 = 42;
+	int var2 = ~var1;
+	
+	System.out.println(var1 + "  " +var2);
+}
+
+}
---
+++ 21-jun/Demo4.java
@@ -0,0 +1,11 @@
+public class Demo4{
+
+  public static void main(String x[]){
+
+	int a = 3, b = 6;
+	int c = a | b;
+	int d = a & b;
+
+	System.out.println(c + "\t" +d);
+}
+}
